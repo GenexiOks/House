10bc22ee88b67f1995686c7f5bc867b645a9a2fc
Genexi <Pashaz1994@gmail.com>
2024-11-27 09:06:44 +0300

    27/11/24 home

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,65 @@
 import styles from "./Card.module.css";
 import Painting from "./Painting.jsx";
 
+const France = [
+    {
+        img: "/Card/France/01.png",
+        author: "Марсель Руссо",
+        title_work: "Охота Амура",
+        about: "Холст, масло (50х80) ",
+        cost: "14 500 руб",
+    },
+    {
+        img: "/Card/France/02.png",
+        author: "Анри Селин",
+        title_work: "Дама с собачкой",
+        about: "Акрил, бумага (50х80)",
+        cost: "16 500 руб",
+    },
+    {
+        img: "/Card/France/03.png",
+        author: "Франсуа Дюпон",
+        title_work: "Процедура",
+        about: "Цветная литография (40х60) ",
+        cost: "20 000 руб",
+    },
+    {
+        img: "/Card/France/04.png",
+        author: "Луи Детуш",
+        title_work: "Роза",
+        about: "Бумага, акрил (50х80) ",
+        cost: "12 000 руб",
+    },
+    {
+        img: "/Card/France/05.png",
+        author: "Луи Детуш",
+        title_work: "Птичья трапеза",
+        about: "Цветная литография (40х60) ",
+        cost: "22 500 руб",
+    },
+    {
+        img: "/Card/France/06.png",
+        author: "Пьер Моранж",
+        title_work: "Пейзаж с рыбой",
+        about: "Цветная литография (40х60) ",
+        cost: "20 000 руб",
+    },
+];
+
 function Card() {
+    const card = France.map((value) => (
+        <Painting
+            img={value.img}
+            author={value.author}
+            title_work={value.title_work}
+            about={value.about}
+            cost={value.cost}
+        />
+    ));
+
     return (
         <>
-            <div className={styles.card__container}>
-                <Painting />
-            </div>
+            <div className={styles.card__container}>{card}</div>
         </>
     );
 }
--- src/components/Painting.jsx
+++ src/components/Painting.jsx
@@ -1,10 +1,15 @@
 import styles from "./Painting.module.css";
 
-function Painting() {
+function Painting(props) {
     return (
         <>
             <div className={styles.painting__container}>
-                <img src="/Card/France/03.png" alt="#" />
+                <img className={styles.paintin__img} src={props.img} alt="#" />
+                <p className={styles.painting__author}>{props.author}</p>
+                <h1 className={styles.painting__title_work}>{props.title_work}</h1>
+                <p className={styles.painting__about}>{props.about}</p>
+                <p className={styles.painting__cost}>{props.cost}</p>
+                <button className={styles.painting__btn}>В корзину</button>
             </div>
         </>
     );
