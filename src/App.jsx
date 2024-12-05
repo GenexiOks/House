72a3fd0033838f0270de31b64258363c1a415771
Pavel <Pashaz1994@gmail.com>
2024-12-05 17:28:32 +0300

    05/12/24 work

--- src/App.jsx
+++ src/App.jsx
@@ -3,144 +3,13 @@ import styles from "./App.module.css";
 import { useState } from "react";
 import Card from "./components/Card.jsx";
 
-const france = [
-    {
-        img: "/Card/France/01.png",
-        author: "Марсель Руссо",
-        title_work: "Охота Амура",
-        about: "Холст, масло (50х80) ",
-        cost: "14 500 руб",
-    },
-    {
-        img: "/Card/France/02.png",
-        author: "Анри Селин",
-        title_work: "Дама с собачкой",
-        about: "Акрил, бумага (50х80)",
-        cost: "16 500 руб",
-    },
-    {
-        img: "/Card/France/03.png",
-        author: "Франсуа Дюпон",
-        title_work: "Процедура",
-        about: "Цветная литография (40х60) ",
-        cost: "20 000 руб",
-    },
-    {
-        img: "/Card/France/04.png",
-        author: "Луи Детуш",
-        title_work: "Роза",
-        about: "Бумага, акрил (50х80) ",
-        cost: "12 000 руб",
-    },
-    {
-        img: "/Card/France/05.png",
-        author: "Луи Детуш",
-        title_work: "Птичья трапеза",
-        about: "Цветная литография (40х60) ",
-        cost: "22 500 руб",
-    },
-    {
-        img: "/Card/France/06.png",
-        author: "Пьер Моранж",
-        title_work: "Пейзаж с рыбой",
-        about: "Цветная литография (40х60) ",
-        cost: "20 000 руб",
-    },
-];
-
-const germany = [
-    {
-        img: "/Card/germany/01.png",
-        author: "Курт Вернер",
-        title_work: "Над городом",
-        about: "Цветная литография (40х60) ",
-        cost: "16 000 руб",
-    },
-    {
-        img: "/Card/germany/02.png",
-        author: "Макс Рихтер",
-        title_work: "Птенцы",
-        about: "Холст, масло (50х80) ",
-        cost: "14 500 руб",
-    },
-    {
-        img: "/Card/germany/03.png",
-        author: "Мартин Майер",
-        title_work: "Среди листьев",
-        about: "Цветная литография (40х60) ",
-        cost: "20 000 руб",
-    },
-    {
-        img: "/Card/germany/04.png",
-        author: "Герман Беккер",
-        title_work: "Яркая птица",
-        about: "Цветная литография (40х60) ",
-        cost: "13 000 руб",
-    },
-    {
-        img: "/Card/germany/05.png",
-        author: "Вульф Бауэр",
-        title_work: "Дятлы",
-        about: "Бумага, акрил (50х80) ",
-        cost: "20 000 руб",
-    },
-    {
-        img: "/Card/germany/06.png",
-        author: "Вальтер Хартманн",
-        title_work: "Большие воды",
-        about: "Бумага, акрил (50х80) ",
-        cost: "23 000 руб",
-    },
-];
-
-const england = [
-    {
-        img: "/Card/england/01.png",
-        author: "Пол Смит",
-        title_work: "Дикий зверь",
-        about: "Акварель, бумага (50х80)  ",
-        cost: "19 500 руб",
-    },
-    {
-        img: "/Card/england/02.png",
-        author: "Джон Уайт",
-        title_work: "Скалистый берег",
-        about: "Цветная литография (40х60) ",
-        cost: "17 500 руб",
-    },
-    {
-        img: "/Card/england/03.png",
-        author: "Джим Уотсон",
-        title_work: "Река и горы",
-        about: "Акварель, бумага (50х80) ",
-        cost: "20 500 руб",
-    },
-    {
-        img: "/Card/england/04.png",
-        author: "Юджин Зиллион",
-        title_work: "Белый попугай",
-        about: "Цветная литография (40х60) ",
-        cost: "15 500 руб",
-    },
-    {
-        img: "/Card/england/05.png",
-        author: "Эрик Гиллман",
-        title_work: "Ночная рыба",
-        about: "Бумага, акрил (50х80) ",
-        cost: "12 500 руб",
-    },
-    {
-        img: "/Card/england/06.png",
-        author: "Альфред Барр",
-        title_work: "Рыжий кот",
-        about: "Цветная литография (40х60) ",
-        cost: "21 000 руб",
-    },
-];
-
 function App() {
     const [hiddenMenu, setHiddenMenu] = useState(false);
-    const [country, setCountry] = useState(france);
+    const [activeTab, setActiveTab] = useState(0);
+
+    function openTab(e) {
+        setActiveTab(e.target.id);
+    }
 
     function hiddenHandler() {
         hiddenMenu ? setHiddenMenu(false) : setHiddenMenu(true);
@@ -296,35 +165,19 @@ function App() {
                     <div className={classNames(classNames(styles.catalog__container, styles._container))}>
                         <div className={classNames(styles.catalog__menu, styles._container)}>
                             <h1 className={styles.catalog__title}>Репродукции</h1>
-                            <nav className={styles.catalog__btn_container}>
-                                <button
-                                    autoFocus
-                                    onClick={function () {
-                                        setCountry(france);
-                                    }}
-                                    className={classNames(styles.catalog__btn, styles.catalog__btn_france)}
-                                >
+                            <nav className={styles.btn_container}>
+                                <button id="0" onClick={openTab} className={classNames(styles.btn, styles.btn_france, styles._active)}>
                                     Франция
                                 </button>
-                                <button
-                                    onClick={function () {
-                                        setCountry(germany);
-                                    }}
-                                    className={classNames(styles.catalog__btn, styles.catalog__btn_germany)}
-                                >
+                                <button id="1" onClick={openTab} className={classNames(styles.btn, styles.btn_germany)}>
                                     Гeрмания
                                 </button>
-                                <button
-                                    onClick={function () {
-                                        setCountry(england);
-                                    }}
-                                    className={classNames(styles.catalog__btn, styles.catalog__btn_england)}
-                                >
+                                <button id="2" onClick={openTab} className={classNames(styles.btn, styles.btn_england)}>
                                     Англия
                                 </button>
                             </nav>
                         </div>
-                        <Card country={country} />
+                        <Card country={activeTab} />
                     </div>
                 </div>
             </main>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,144 @@
 import styles from "./Card.module.css";
 import Painting from "./Painting.jsx";
 
+const countryData = [
+    [
+        {
+            img: "/Card/France/01.png",
+            author: "Марсель Руссо",
+            title_work: "Охота Амура",
+            about: "Холст, масло (50х80) ",
+            cost: "14 500 руб",
+        },
+        {
+            img: "/Card/France/02.png",
+            author: "Анри Селин",
+            title_work: "Дама с собачкой",
+            about: "Акрил, бумага (50х80)",
+            cost: "16 500 руб",
+        },
+        {
+            img: "/Card/France/03.png",
+            author: "Франсуа Дюпон",
+            title_work: "Процедура",
+            about: "Цветная литография (40х60) ",
+            cost: "20 000 руб",
+        },
+        {
+            img: "/Card/France/04.png",
+            author: "Луи Детуш",
+            title_work: "Роза",
+            about: "Бумага, акрил (50х80) ",
+            cost: "12 000 руб",
+        },
+        {
+            img: "/Card/France/05.png",
+            author: "Луи Детуш",
+            title_work: "Птичья трапеза",
+            about: "Цветная литография (40х60) ",
+            cost: "22 500 руб",
+        },
+        {
+            img: "/Card/France/06.png",
+            author: "Пьер Моранж",
+            title_work: "Пейзаж с рыбой",
+            about: "Цветная литография (40х60) ",
+            cost: "20 000 руб",
+        },
+    ],
+    [
+        {
+            img: "/Card/germany/01.png",
+            author: "Курт Вернер",
+            title_work: "Над городом",
+            about: "Цветная литография (40х60) ",
+            cost: "16 000 руб",
+        },
+        {
+            img: "/Card/germany/02.png",
+            author: "Макс Рихтер",
+            title_work: "Птенцы",
+            about: "Холст, масло (50х80) ",
+            cost: "14 500 руб",
+        },
+        {
+            img: "/Card/germany/03.png",
+            author: "Мартин Майер",
+            title_work: "Среди листьев",
+            about: "Цветная литография (40х60) ",
+            cost: "20 000 руб",
+        },
+        {
+            img: "/Card/germany/04.png",
+            author: "Герман Беккер",
+            title_work: "Яркая птица",
+            about: "Цветная литография (40х60) ",
+            cost: "13 000 руб",
+        },
+        {
+            img: "/Card/germany/05.png",
+            author: "Вульф Бауэр",
+            title_work: "Дятлы",
+            about: "Бумага, акрил (50х80) ",
+            cost: "20 000 руб",
+        },
+        {
+            img: "/Card/germany/06.png",
+            author: "Вальтер Хартманн",
+            title_work: "Большие воды",
+            about: "Бумага, акрил (50х80) ",
+            cost: "23 000 руб",
+        },
+    ],
+    [
+        {
+            img: "/Card/england/01.png",
+            author: "Пол Смит",
+            title_work: "Дикий зверь",
+            about: "Акварель, бумага (50х80)  ",
+            cost: "19 500 руб",
+        },
+        {
+            img: "/Card/england/02.png",
+            author: "Джон Уайт",
+            title_work: "Скалистый берег",
+            about: "Цветная литография (40х60) ",
+            cost: "17 500 руб",
+        },
+        {
+            img: "/Card/england/03.png",
+            author: "Джим Уотсон",
+            title_work: "Река и горы",
+            about: "Акварель, бумага (50х80) ",
+            cost: "20 500 руб",
+        },
+        {
+            img: "/Card/england/04.png",
+            author: "Юджин Зиллион",
+            title_work: "Белый попугай",
+            about: "Цветная литография (40х60) ",
+            cost: "15 500 руб",
+        },
+        {
+            img: "/Card/england/05.png",
+            author: "Эрик Гиллман",
+            title_work: "Ночная рыба",
+            about: "Бумага, акрил (50х80) ",
+            cost: "12 500 руб",
+        },
+        {
+            img: "/Card/england/06.png",
+            author: "Альфред Барр",
+            title_work: "Рыжий кот",
+            about: "Цветная литография (40х60) ",
+            cost: "21 000 руб",
+        },
+    ],
+];
+
 function Card(props) {
-    const countryData = props.country;
-    const card = countryData.map((value, i) => <Painting img={value.img} author={value.author} title_work={value.title_work} about={value.about} cost={value.cost} key={i} />);
+    const country = countryData[props.country];
+    const card = country.map((value, i) => <Painting img={value.img} author={value.author} title_work={value.title_work} about={value.about} cost={value.cost} key={i} />);
 
     return (
         <>
